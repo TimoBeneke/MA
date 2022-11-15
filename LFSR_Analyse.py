d08fe57334f10524190e045f3cc4ac1029bdb87b
Timo Beneke <beneke.timo@gmail.com>
2022-11-15 21:22:53 +0100

    Übersichtlichkeit

--- LFSR_Analyse.py
+++ LFSR_Analyse.py
@@ -1,6 +1,8 @@
 state = 0b1000010010110001000011
 length = 4194304
 
+# LFSR der Form (0, 1, 22)
+
 def lfsr(length, state):
     outputs = []
     for i in range(length):
@@ -13,7 +15,8 @@ def lfsr(length, state):
 result = lfsr(length, state)
 print(len(result))
 
-numOne = 0
+# Anzahl Einsen und Nullen für outputs.append(state & 1)
+"""numOne = 0
 numZero = 0
 
 for i in result:
@@ -23,8 +26,7 @@ for i in result:
         numZero += 1
 
 print ("Number of Zeroes: " + str(numZero))
-print ("Number of Ones: " + str(numOne))
+print ("Number of Ones: " + str(numOne))"""
 
 if result[0] == result[-1]:
     print("Maximum Period")
-0, 1, 22
